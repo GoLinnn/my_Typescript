b914f4995f583d69c54436da6189becd735c4c06
GoLinnn <2402114737@qq.com>
2024-01-11 14:54:41 +0800

    02基本输入显示功能

--- 02SimpleTodoList/src/main.ts
+++ 02SimpleTodoList/src/main.ts
@@ -1,24 +1,30 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// 声明listItem的类型，用type关键字
+type ListItem = {
+  id: number,
+  item: string,
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// 获取提交按钮
+const form = document.getElementById('form') as HTMLFormElement
+
+form.addEventListener('submit', (event: SubmitEvent) => {
+  // 消除默认跳转效果
+  event.preventDefault()
+
+  // 获取输入框的内容
+  const input = document.getElementById('input') as HTMLInputElement
+
+  const listItem: ListItem = {
+    // 利用日期获取随机时间
+    id: new Date().getTime(),
+    item: input.value
+  }
+
+  const ul = document.getElementById('list') as HTMLUListElement;
+  const li = document.createElement('li') as HTMLLIElement
+  li.innerHTML = listItem.item
+  ul.append(li)
+})
+
