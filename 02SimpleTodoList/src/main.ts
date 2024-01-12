e13164690a8badb9170bd44e3c9705ad40c8f540
GoLinnn <2402114737@qq.com>
2024-01-12 09:50:30 +0800

    02功能实现

--- 02SimpleTodoList/src/main.ts
+++ 02SimpleTodoList/src/main.ts
@@ -2,9 +2,12 @@ import './style.css'
 
 // 声明listItem的类型，用type关键字
 type ListItem = {
-  id: number,
+  id: string,
   item: string,
 }
+
+// 在整个页面（或者文档）被完全加载和解析完成后（也就是全部的DOM构建完成后），执行render函数。
+document.addEventListener('DOMContentLoaded', render)
 
 // 获取提交按钮
 const form = document.getElementById('form') as HTMLFormElement
@@ -18,13 +21,43 @@ form.addEventListener('submit', (event: SubmitEvent) => {
 
   const listItem: ListItem = {
     // 利用日期获取随机时间
-    id: new Date().getTime(),
+    id: new Date().getTime().toString(),
     item: input.value
   }
 
-  const ul = document.getElementById('list') as HTMLUListElement;
-  const li = document.createElement('li') as HTMLLIElement
-  li.innerHTML = listItem.item
-  ul.append(li)
+  // 将输入存储到本地
+  localStorage.setItem(listItem.id, listItem.item)
+
+  render()
+  // 清空输入框内容
+  input.value = ''
 })
+
+// 渲染函数
+function render(): void {
+  const ul = document.getElementById('list') as HTMLUListElement
+  ul.innerHTML = "";  // 清空列表
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let itemKey = localStorage.key(i)
+    // getItem需要的参数是字符串，要先判断itemKey是否为空
+    if (itemKey !== null) {
+      let itemValue = localStorage.getItem(itemKey?.toString())
+
+      if (itemValue === null) return
+      // 添加li
+      const li = document.createElement('li') as HTMLLIElement
+      li.innerHTML = itemValue
+      ul.append(li)
+      // 添加删除按钮
+      const btn_delete = document.createElement('button') as HTMLButtonElement
+      btn_delete.textContent = 'X'
+      btn_delete.addEventListener('click', () => {
+        localStorage.removeItem(itemKey!) //!非空断言操作符，已经检查过itemKey不为null.
+        render() // 从localStorage移除项后，重新渲染列表
+      })
+      li.append(btn_delete)
+    }
+  }
+}
 
